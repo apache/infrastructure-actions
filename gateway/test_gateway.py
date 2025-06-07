146ebc343b5628cf729c5b1a2c720e3219835eeb
Arnout Engelen <engelen@apache.org>
2025-06-07 04:15:26 +0200

    refactor(gateway): migrate to ruyaml (#169)
    * Run tests for PRs
    
    There are some specific tests for the stash action in
    `.github/workflows/stash-action-test.yml`. These might
    test particular scenario's, but we should also run the
    'default' testset that includes the gateway tests
    
    * gateway: migrate to ruyaml
    
    in preparation of supporting tag comments
    
    * Update .github/workflows/test.yml
    
    Signed-off-by: Jacob Wujciak-Jens <jacob@wujciak.de>
    
    * tweak test workflow
    
    * fix job name
    
    ---------
    
    Signed-off-by: Jacob Wujciak-Jens <jacob@wujciak.de>
    Co-authored-by: Jacob Wujciak-Jens <jacob@wujciak.de>

--- gateway/test_gateway.py
+++ gateway/test_gateway.py
@@ -1,6 +1,22 @@
 import datetime
 
+import filecmp
 from gateway import *
+
+
+def test_load_yaml():
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    parsed = load_yaml(this_dir + "/test_dummy.yml")
+    comment = parsed['jobs']['dummy']['steps'][3].ca.items['uses'][2].value
+    assert comment == "# v4.6.8\n"
+
+def test_roundtrip_yaml():
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    infile = this_dir + "/test_dummy.yml"
+    parsed = load_yaml(infile)
+    outfile = this_dir + "/test_out_dummy.yml"
+    write_yaml(outfile, parsed)
+    assert filecmp.cmp(infile, outfile, shallow=False)
 
 
 def test_update_refs():
@@ -38,7 +54,7 @@ def test_update_refs():
         },
         "dorny/paths-filter": {
             "0bc4621a3135347011ad047f9ecf449bf72ce2bd": {
-                "expires_at": calculate_expiry()
+                "expires_at": calculate_expiry(12)
             },
             "de90cc6fb38fc0963ad72b210f1f284cd68cea36": {
                 "expires_at": datetime.date(2100, 1, 1),
